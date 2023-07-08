5c50f4d6a54ac6cacd4346fe992d40828ba3d496
Sankalp Pawar <109941967+Sanku1299@users.noreply.github.com>
2023-07-08 11:53:26 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+
+// import routes and route
+import { Routes, Route } from "react-router-dom";
+
+// import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// import pages
+import Home from './pages/Home';
+import PropertyDetails from './pages/PropertyDetails';
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
